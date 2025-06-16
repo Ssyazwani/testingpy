bdf8ac9a2f93fb982f48fbf594458f37f164e426
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-06-16 21:57:46 +0800

    finish nutrition

--- cs50intro/lecture2/nutrition.py
+++ cs50intro/lecture2/nutrition.py
@@ -1,10 +1,47 @@
-inputCal = input()
 
-fruit_prices = {
-    "apple": 3,
-    "banana": 2,
-    "orange": 4
+fruit_cal = {
+    "apple": 130,
+    "avocado": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80,
 }
+
+
+
+fruit = input("Item: ").strip().lower()  # Prompt and input on same line
+
+while fruit not in fruit_cal:
+    fruit = input().lower()
+
+print("Calories:"+ str(fruit_cal[fruit])) 
+
+
+
+
+
+# if fruit not in fruit_cal:
+#         continue 
+
+
+# if fruit in fruit_cal:
+#         print("Calories:"+ str(fruit_cal[fruit]))
+#      break  
 
 
 # Apple
