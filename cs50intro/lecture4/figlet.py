c2ffa68ff8e12bd31ed57f24cb62a7bb7dbb21e8
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-06-24 15:41:09 +0800

    did figlet.py

---
+++ cs50intro/lecture4/figlet.py
@@ -0,0 +1,23 @@
+from pyfiglet import Figlet, FigletFont
+import sys
+
+args = sys.argv[1:]
+
+if len(args) == 0:
+    font = 'standard'  
+
+elif len(args) == 2 and args[0] in ['-f', '--font']:
+    font = args[1]
+    if font not in Figlet().getFonts():
+        sys.exit("Invalid Usage")
+else:
+    sys.exit("Usage: python figlet.py [-f FONT_NAME]")
+
+
+figlet = Figlet(font = font)
+text = input("Input: ")
+print("Output:")
+print(figlet.renderText(text))
+
+# f = Figlet(font='slant')
+# print(f.renderText('text to render'))
